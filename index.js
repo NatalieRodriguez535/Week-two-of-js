035139add29ac404cc723924b732e69e12b615c2
Natalie Rodriguez <72086492+NatalieRodriguez535@users.noreply.github.com>
2020-11-09 17:52:18 -0500

    update

--- index.js
+++ index.js
@@ -21,52 +21,29 @@ var months = ["That is not a month.", "January", "February", "March", "April", "
 
 if(userInput === 1){
     console.log( months[1])
-}
-if(userInput === 2){
+}else if(userInput === 2){
     console.log( months[2])
-}
-
-if(userInput === 3){
+}else if(userInput === 3){
     console.log( months[3])
-}
-
-if(userInput === 4){
+}else if(userInput === 4){
     console.log( months[4])
-}
-
-if(userInput === 5){
+}else if(userInput === 5){
     console.log( months[5])
-}
-
-if(userInput === 6){
+}else if(userInput === 6){
     console.log( months[6])
-}
-
-if(userInput === 7){
+}else if(userInput === 7){
     console.log( months[7])
-}
-
-if(userInput === 8){
+}else if(userInput === 8){
     console.log( months[8])
-}
-
-if(userInput === 9){
+}else if(userInput === 9){
     console.log( months[9])
-}
-
-if(userInput === 10){
+}else if(userInput === 10){
     console.log( months[10])
-}
-
-if(userInput === 11){
+}else if(userInput === 11){
     console.log( months[11])
-}
-
-if(userInput === 12){
+}else if(userInput === 12){
     console.log( months[12])
-}
-
-else{
+}else{
     console.log( months[0])
 }
 
@@ -78,7 +55,7 @@ const heightJerry = 10;
 const BMITom = massTom / heightTom ** 2;
 const BMIJerry = massJerry / (heightJerry * heightJerry);
 const higherBMI = BMITom > BMIJerry;
-if (BMIJerry < BMITom) {
+else if (BMIJerry < BMITom) {
     console.log(`Tom's BMI ${BMITom} is higher than Jerry's ${BMIJerry}`)
 } else {
     console.log(`Jerry's BMI (${BMIJerry}) is higher than Tom's (${BMITom})`)
